0ba90484c89e8dc3e07af0d4635eb865d59262fd
Sithija Shehara <nsithijashehara@gmail.com>
2020-12-05 13:23:11 +0530

    Added basic functionality without Backend.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    "Work with React + Firebase",
+    "Work with Angular + Node & Mongo",
+  ]);
+
+  const [input, setInput] = useState("");
+
+  const addToDo = (event) => {
+    setTodos([...todos, input]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Sithija</h1>
+      <input value={input} onChange={(event) => setInput(event.target.value)} />
+      <button onClick={addToDo}> Add ToDo </button>
+
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
