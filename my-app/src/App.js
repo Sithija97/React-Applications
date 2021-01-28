3d4e6b5f149ec5a0414b7cdb23943ee5ff95e9bf
Sithija Shehara <nsithijashehara@gmail.com>
2021-01-28 23:15:46 +0530

    Data getting from the form and displaying it

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,19 @@
+import React, { useState } from 'react'
 import './App.css';
 import Header from './componets/Header'
 import AddContact from './componets/AddContact'
 import ContactList from './componets/ContactList'
 
 function App() {
-
-  const contacts = [
-    {
-      id:"1",
-      name:"Sithija",
-      email: "sithi@gmail.com"
-    },
-    {
-      id:"2",
-      name:"Shehara",
-      email: "shee@gmail.com"
-    }
-  ]
+  const [contacts, setContacts] = useState([])
+  const addContactHandler = (contact)=> {
+    console.log(contact)
+  }
   return (
     <div className='ui container'>
-      <Header/>
-      <AddContact/>
-      <ContactList contacts={contacts}/>
+      <Header />
+      <AddContact addContactHandler={addContactHandler}/>
+      <ContactList contacts={contacts} />
     </div>
   );
 }
--- my-app/src/componets/ContactList.js
+++ my-app/src/componets/ContactList.js
@@ -1,16 +1,11 @@
 import React from 'react'
+import ContactCard from './ContactCard'
 
 const ContactList = (props) => {
     //function to get contacts
-    const getContactList = props.contacts.map((contact)=>{
-        return(
-            <div className="item">
-                <div className="content">
-                    <div className="header">{contact.name}</div>
-                    <div>{contact.email}</div>
-                </div>
-                <i className="trash alternate outline icon"></i>
-            </div>
+    const getContactList = props.contacts.map((contact) => {
+        return (
+            <ContactCard contact={contact}/>
         )
     })
     return (
