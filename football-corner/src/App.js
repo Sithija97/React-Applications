e869ad888849b07f1a8a98d1ff4ec28430ce73c8
Sithija Shehara <nsithijashehara@gmail.com>
2021-02-03 00:28:37 +0530

    UI building process..

--- football-corner/src/App.js
+++ football-corner/src/App.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
 import './App.css';
+import { Container, Typography } from '@material-ui/core';
 import Axios from "axios";
 
 //importing components
-import Root from "./components/Root";
+import Root from "./components/Body/Root";
 
 function App() {
   return (
-    <div>
-      <Root/>
+    <div className='App'>
+      <Container maxWidth='100%'  style={{background:'#03a9f4', height:'100vh'}}>Sithija</Container>
+      <Container maxWidth='100%'  style={{background:'white', height:'100vh'}}>Sithija</Container>
+      <Container maxWidth='100%'  style={{height:'100vh'}}>Sithija</Container>
     </div>
   );
 }
