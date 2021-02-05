3a8aa1b4abd61cb8d8826a3d0f08b238d2d27edb
Sithija Shehara <nsithijashehara@gmail.com>
2021-02-05 23:00:52 +0530

    Initialized a news app.

---
+++ news-app/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div class="container">
+      <div className="row">
+        <div class="col-sm">
+          <div class="card" style={{ width: '18rem', margin:'10px' }}>
+            <img src="..." class="card-img-top" alt="..." />
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
