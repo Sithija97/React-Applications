9d9cb53aeaf6af12a02aa58ebc5473be51f51b8b
Sithija Shehara <nsithijashehara@gmail.com>
2021-02-07 22:05:04 +0530

    Code change

--- news-app/src/App.js
+++ news-app/src/App.js
@@ -1,18 +1,15 @@
 
 import './App.css';
+import NewsCard from './components/NewsCard' 
 
 function App() {
   return (
     <div class="container">
       <div className="row">
-        <div class="col-sm">
-          <div class="card" style={{ width: '18rem', margin:'10px' }}>
-            <img src="..." class="card-img-top" alt="..." />
-            <div class="card-body">
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-          </div>
-        </div>
+        <NewsCard/>
+        <NewsCard/>
+        <NewsCard/>
+        <NewsCard/>
       </div>
     </div>
   );
---
+++ news-app/src/components/NewsCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NewsCard = () => {
+    return (
+        <div class="col-sm">
+          <div class="card" style={{ width: '18rem', margin:'10px' }}>
+            <img src="..." class="card-img-top" alt="..." />
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+    )
+}
+export default NewsCard;
