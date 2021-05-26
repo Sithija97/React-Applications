64c6f644f12f70836bdb56ed09c7009409b2e8f1
Sithija Shehara <nsithijashehara@gmail.com>
2021-05-26 12:30:45 +0530

    Simple Socialmedia Login Completed

--- react-auth/src/components/Home.js
+++ react-auth/src/components/Home.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { auth } from "../firebase";
 
-const Home = () => {
-    return (
-        <div>
-            <h2>Home</h2>
-        </div>
-    )
-}
+function Home() {
+  var user;
+  useEffect(() => {
+    user = auth.currentUser;
+    console.log(user)
+  }, [auth])
+  const signOut = () => {
+    auth
+      .signOut()
+        .then(() => {
+        // Sign-out successful.
+        console.log("Sign-out successful.");
+      })
+      .catch((error) => {
+        // An error happened.
+        console.log(" An error happened.", error);
+      });
+  };
+  return (
+    <div>
+      <h2>Home</h2>
+      <h3>Hi</h3>
+      <button onClick={signOut}>log out</button>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- react-auth/src/components/Login.js
+++ react-auth/src/components/Login.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../App.css'
 import { auth, providerGoogle, providerFacebook, providerGithub } from '../firebase'
 import GoogleButton from "react-google-button";
-const signInWithGoogle = () => {
+import Home from './Home';
+
+const Login = () => {
+  const signInWithGoogle = () => {
     auth
       .signInWithPopup(providerGoogle)
       .then((result) => {
@@ -12,9 +15,9 @@ const signInWithGoogle = () => {
         // This gives you a Google Access Token. You can use it to access the Google API.
         var token = credential.accessToken;
         // The signed-in user info.
-          var user = result.user;
+        var user = result.user;
         // display user
-          console.log('user', user.displayName)
+        //console.log("user", user);
       })
       .catch((error) => {
         // Handle Errors here.
@@ -25,23 +28,23 @@ const signInWithGoogle = () => {
         // The firebase.auth.AuthCredential type that was used.
         var credential = error.credential;
       });
-}
+  };
 
-const signInWithFacebook = () => {
+  const signInWithFacebook = () => {
     auth
-        .signInWithPopup(providerFacebook)
-        .then((result) => {
+      .signInWithPopup(providerFacebook)
+      .then((result) => {
         /** @type {firebase.auth.OAuthCredential} */
         var credential = result.credential;
 
         // The signed-in user info.
         var user = result.user;
         // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-        var accessToken = credential.accessToken;        
+        var accessToken = credential.accessToken;
         // display user
-        console.log('user', user)
-        })
-        .catch((error) => {
+        //console.log("user", user);
+      })
+      .catch((error) => {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -50,53 +53,75 @@ const signInWithFacebook = () => {
         // The firebase.auth.AuthCredential type that was used.
         var credential = error.credential;
         console.log("errorMessage", errorMessage);
-    });
-}
+      });
+  };
 
-const signInWithGithub = () => {
-  auth
-    .signInWithPopup(providerGithub)
-    .then((result) => {
-      /** @type {firebase.auth.OAuthCredential} */
-      var credential = result.credential;
+  const signInWithGithub = () => {
+    auth
+      .signInWithPopup(providerGithub)
+      .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential = result.credential;
 
-      // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-      var token = credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      // display user
-      console.log('user', user)
-    })
-    .catch((error) => {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      console.log("errorMessage", errorMessage);
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        var token = credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // display user
+        //console.log("user", user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        console.log("errorMessage", errorMessage);
+      });
+  };
+
+  const initiaStateValues = {
+    islogIn: false,
+    name : ''
+  }
+
+  const [state, setState] = useState(initiaStateValues)
+
+  useEffect(() => {
+    auth.onAuthStateChanged(function (user) {
+      if (user) {
+        // User is signed in.
+        console.log("user signed in");
+        //console.log("user", user);
+
+        //setting the state
+        setState({
+          islogIn: true,
+          name : user.displayName
+        })
+
+        //get the current user
+        // var user = firebase.auth().currentUser;
+      } else {
+        // No user is signed in.
+        console.log("user signed in");
+      }
     });
-}
-const signOut = () => {
-  auth
-    .signOut()
-    .then(() => {
-      // Sign-out successful.
-      console.log("Sign-out successful.");
-    })
-    .catch((error) => {
-      // An error happened.
-      console.log(" An error happened.", error);
-    });
-}
-const Login = () => {
+  }, [auth])
     return (
       <div className="App">
-        <button onClick={signInWithGoogle}>Sign in with Google</button>
-        <button onClick={signInWithFacebook}>Sign in with Facebook</button>
-        <button onClick={signInWithGithub}>Sign in with Github</button>
-        <button onClick={signOut}>log out</button>
+        {state.islogIn === false ? (
+          <div>
+            <button onClick={signInWithGoogle}>Sign in with Google</button>
+            <button onClick={signInWithFacebook}>Sign in with Facebook</button>
+            <button onClick={signInWithGithub}>Sign in with Github</button>
+          </div>
+        ) : (
+            <Home/>
+        )}
+
         {/* <div>
           <GoogleButton onClick={signInWithGoogle} />
         </div>
