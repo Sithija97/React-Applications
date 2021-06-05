2e85f9904197cd6539061527ee4d9bf1f86b4050
Sithija Shehara <nsithijashehara@gmail.com>
2021-06-05 18:19:28 +0530

    Basic Profile Card

--- expense-manager/expense-app/src/components/Home.js
+++ expense-manager/expense-app/src/components/Home.js
@@ -1,39 +1,31 @@
 import './Home.css'
 import React, { useContext, useEffect } from "react";
 import { auth } from "../firebase";
-import { UserContext } from "../StateContext";
-import { useHistory } from "react-router-dom";
 import SectionOne from './sub-components/SectionOne';
 import SectionTwo from './sub-components/SectionTwo';
 import SectionThree from './sub-components/SectionThree';
 
 function Home () {
-  const [user, setUser] = useContext(UserContext);
-  const history = useHistory();
-
-  const signOut = () => {
-    auth
-      .signOut()
-      .then(() => {
-        setUser(null);
-        // Sign-out successful.
-        history.push("/login");
-        console.log("Sign-out successful.");
-      })
-      .catch((error) => {
-        // An error happened.
-        console.log(" An error happened.", error);
-      });
-  };
+  // const signOut = () => {
+  //   auth
+  //     .signOut()
+  //     .then(() => {
+  //       setUser(null);
+  //       // Sign-out successful.
+  //       history.push("/login");
+  //       console.log("Sign-out successful.");
+  //     })
+  //     .catch((error) => {
+  //       // An error happened.
+  //       console.log(" An error happened.", error);
+  //     });
+  // };
   return (
     <div className='app'>
       {/* <h2>Home</h2> */}
       <SectionOne />
       <SectionTwo />
       <SectionThree/>
-      {/* <img src={user === null ? " " : user.photoURL} alt="" />
-      <h3>Hi {user === null ? " " : user.displayName} </h3>
-      <button onClick={signOut}>log out</button> */}
     </div>
   );
 };
--- expense-manager/expense-app/src/components/sub-components/SectionThree.js
+++ expense-manager/expense-app/src/components/sub-components/SectionThree.js
@@ -1,23 +1,34 @@
-import React from 'react'
+import React, { useContext } from "react";
 import "./SectionStyles.css";
-import { Card, CardContent } from "@material-ui/core";
+import { Avatar, Card, CardContent } from "@material-ui/core";
+import { UserContext } from "../../StateContext";
+import { useHistory } from "react-router-dom";
 
 function SectionThree() {
-    return (
-      <div className="section_three">
-        <Card className="section_three_profile_card">
-          <CardContent>
-            <p>sec 3</p>
-          </CardContent>
-        </Card>
-        <br></br>
-        <Card className="section_three_form_card">
-          <CardContent>
-            <p>sec 3</p>
-          </CardContent>
-        </Card>
-      </div>
-    );
+  const [user, sertUser] = useContext(UserContext);
+  return (
+    <div className="section_three">
+      <Card className="section_three_profile_card">
+        <h4>Profile</h4>
+        <CardContent className="prfile_content">
+          <img
+            className="profile_picture"
+            src={user === null ? <Avatar></Avatar> : user.photoURL}
+            alt=""
+          />
+          <div className="profile_name">
+            <b>{user === null ? " " : user.displayName}</b>
+            <br></br>
+            {user === null ? " " : user.email}
+          </div>
+        </CardContent>
+      </Card>
+      <br></br>
+      <Card className="section_three_form_card">
+        <CardContent></CardContent>
+      </Card>
+    </div>
+  );
 }
 
-export default SectionThree
+export default SectionThree;
