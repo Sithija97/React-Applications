aeb439b9132ca1db7b8d5c6db67f72a2acc64a23
Sithija Shehara <nsithijashehara@gmail.com>
2021-06-18 21:47:08 +0530

    Frontend basic code with backend controllers

--- memories project/backend-app/index.js
+++ memories project/backend-app/index.js
@@ -2,17 +2,35 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 
+// routes
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors())
+app.use(cors());
 
-const CONNECTION_URL = ''
+const CONNECTION_URL = "mongodb://localhost:27017/memories";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useUnifiedTopology: true, useNewUrlParser: true })
-    .then(()=> app.listen(PORT, ()=>{ console.log(`Server running on port: ${PORT}`)}))
-    .catch((error) => { console.log(error.message) })
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connceted successfully");
+  })
+  .catch((err) => {
+    console.log("err: " + err);
+  });
 
-mongoose.set('useFindAndModify', false)
+app.listen(PORT, () => {
+  console.log("Server connected to port 5000");
+});
+
+mongoose.set("useFindAndModify", false);
