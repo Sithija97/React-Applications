498f4b4cfa1832163036dbf6c1105da6a79a4d2f
Sithija Shehara <nsithijashehara@gmail.com>
2021-06-19 11:44:42 +0530

    Added basic styling to the front

--- memories project/frontend-app/src/App.js
+++ memories project/frontend-app/src/App.js
@@ -1,17 +1,19 @@
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import useStyles from "./styles";
 import memories from "./images/memories.png";
 
 import Posts from './components/Posts/Posts'
 import Form from "./components/Form/Form";
 
 function App() {
+    const classes = useStyles();
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
           Memories
         </Typography>
-        <img src={memories} alt="icon" height="60" />
+        <img className={classes.image} src={memories} alt="icon" height="60" />
       </AppBar>
       <Grow in>
         <Container>
@@ -21,8 +23,12 @@ function App() {
             alignItems="stretch"
             spacing={3}
           >
-            <Grid item xs={12} sm={7}><Posts/></Grid>
-            <Grid item xs={12} sm={7}><Form/></Grid>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <Form />
+            </Grid>
           </Grid>
         </Container>
       </Grow>
