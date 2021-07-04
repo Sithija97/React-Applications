e4a0393cd88e6f2f0489b066ac4fb89951fe9d2e
Sithija Shehara <nsithijashehara@gmail.com>
2021-07-05 00:02:29 +0530

    Getting job list title

---
+++ remotive jobs app/remotive-jobs-app/src/components/JobContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function JobContainer({name}) {
+    return (
+        <div>
+            <p>📱 </p>
+        </div>
+    )
+}
+
+export default JobContainer
--- remotive jobs app/remotive-jobs-app/src/components/Root.js
+++ remotive jobs app/remotive-jobs-app/src/components/Root.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import JobContainer from "./JobContainer";
 
 function Root() {
   const initialState = {
@@ -8,23 +9,31 @@ function Root() {
   };
 
   const [state, setState] = useState(initialState);
-
+  const myArr = [];
   useEffect(async () => {
     await axios
       .get("https://remotive.io/api/remote-jobs?category=software-dev")
-      .then((response) =>
-        setState({
-          loading: false,
-          data: [response.data],
-        })
+      .then(
+        (response) =>
+          setState({
+            data: response.data.jobs,
+          })
+        // console.log(state.data)
       )
       .catch((error) => console.log(error));
+    state.data.map((docs) => {
+      console.log(docs.title)
+    });
   }, []);
-    console.log(state);
-    state.data.map(doc => {
-        doc.jobs.map(job =>{console.log(job)})
-    })
-  return <div>Root</div>;
+
+  return (
+    <div>
+      <h3>Root 📱</h3>
+      {state.data.map((docs) => (
+        <h5>{docs.title}</h5>
+      ))}
+    </div>
+  );
 }
 
 export default Root;
