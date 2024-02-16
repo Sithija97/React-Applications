b5b4162998790c4e0fb9f7127ee59a28535d449b
Sithija Shehara <nsithijashehara@gmail.com>
2024-02-16 22:37:17 +0530

    UI responsiveness fixes

--- marketing agency site 1/src/components/hero.tsx
+++ marketing agency site 1/src/components/hero.tsx
@@ -3,9 +3,9 @@ import heroImg from "../assets/hero.svg";
 export const Hero = () => {
   return (
     <div className="h-full pt-[84px] pb-[122px] pl-[121px] pr-[120.89px]">
-      <div className="grid gap-5 xl:grid-cols-2">
-        <div className="xl:order-first flex items-center justify-start ">
-          <div className="h-[464px] w-[518.11px] bg-white flex flex-col justify-between">
+      <div className="grid  xl:grid-cols-2">
+        <div className="xl:order-first flex items-center justify-start">
+          <div className="h-[464px] xl:w-[518.11px] flex flex-col justify-between">
             <div className="h-[356px] flex flex-col justify-between">
               <p className="font-bold text-[96px] leading-[98.5px]">
                 We Take Care Of Your Brand
@@ -14,21 +14,21 @@ export const Hero = () => {
                 We care about our work and we care about our clients.
               </p>
             </div>
-            <div className="w-[497px] h-[64px] flex gap-2">
+            <div className="h-[64px] flex gap-2">
               <input
                 type="text"
                 placeholder="Enter Your Email"
-                className="bg-slate-50 border border-slate-200 rounded-full py-4 pl-[18px] w-[339px] h-[64px]"
+                className="bg-slate-50 border border-slate-200 rounded-full py-4 pl-[18px] w-full xl:w-[497px] h-[64px]"
               />
-              <button className="flex justify-center items-center bg-blue-700 py-[22px] px-[36px] rounded-full text-[18px] text-center text-slate-100 w-[146px] h-[64px]">
+              <button className="flex justify-center items-center bg-blue-700 py-[22px] px-[36px] rounded-full text-[18px] text-center text-slate-100 w-[200px] xl:w-[166px] h-[64px]">
                 Lets Talk
               </button>
             </div>
           </div>
         </div>
 
-        <div className="order-first bg-white">
-          <img src={heroImg} alt="heroImg" />
+        <div className="order-first ">
+          <img src={heroImg} alt="heroImg" className="m-auto " />
         </div>
       </div>
     </div>
